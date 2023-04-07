ae5a0677561701d84c8cb8757039b704ed54b82c
Janusa <129460842+janusa13@users.noreply.github.com>
2023-04-07 18:08:11 -0300

    Add files via upload

---
+++ mi-primer-projecto-de-node/Ejercicio1 Arrays.js
@@ -0,0 +1,12 @@
+const yo = ["facundo", 23, true];
+console.log(yo);
+
+const fecha = new Date("april 14 1999");
+console.log(fecha);
+
+const libro = {
+  titulo: "El hombre en busca de sentido",
+  autor: "Viktor Frankl",
+  fecha: "1946",
+  url: "https://www.amazon.com/El-hombre-busca-sentido-Spanish/dp/8425432022/ref=sr_1_1?keywords=el+hombre+en+busca+de+sentido+viktor+frankl&qid=1680901474&sprefix=%2Caps%2C336&sr=8-1",
+};
